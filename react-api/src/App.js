a630977689033e9ce10a6b26ca68cccaef3efe3c
Dmitriy Korytnyi <dmitriy.korytnyi@gmail.com>
2020-04-24 19:05:13 +0300

    sass added, template edited

--- react-api/src/App.js
+++ react-api/src/App.js
@@ -1,26 +1,52 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
 
-function App() {
-    return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
-        </div>
-    );
+class App extends React.Component {
+    render () {
+        return (
+            <div className="App">   
+                <div className="w3-sidebar w3-light-grey w3-card-4 w3-animate-left" id="mySidebar">
+                    <div className="w3-bar w3-dark-grey">
+                        <span className="w3-bar-item w3-padding-16">Nav</span>
+                        <button onClick={this.w3_close} className="w3-bar-item w3-button w3-right w3-padding-16"
+                            title="close Sidebar">×</button>
+                    </div>
+                    <div className="w3-bar-block">
+                        <a className="w3-bar-item w3-button w3-green" href="javascript:void(0)">Home</a>
+                        <a className="w3-bar-item w3-button" href="javascript:void(0)">Notes</a>
+                    </div>
+                </div>
+                <div id="main">
+                    <div className="w3-container w3-display-container">
+                        <span title="open Sidebar" id="openNav"
+                            className="w3-button w3-transparent w3-display-topleft w3-xlarge" onClick={this.w3_open}>☰</span>
+                        <h3 className="h3">Slim and React test task</h3>
+                        <div className="content">
+                            Content
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+
+    componentDidMount() {
+        
+    }
+    w3_open = () => {
+        document.getElementById("main").style.marginLeft = "180px";
+        document.getElementById("mySidebar").style.width = "180px";
+        document.getElementById("mySidebar").style.display = "block";
+        document.getElementById("openNav").style.display = 'none';
+    }
+
+    w3_close = () => {
+        document.getElementById("main").style.marginLeft = "0";
+        document.getElementById("mySidebar").style.display = "none";
+        document.getElementById("openNav").style.display = "inline-block";
+    }
+    
 }
+
 
 export default App;
