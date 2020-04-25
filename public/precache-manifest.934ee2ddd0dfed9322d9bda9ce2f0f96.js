d933731c033cf3b3f1683348462ba34ebaaf3a0f
Dmitriy Korytnyi <dmitriy.korytnyi@gmail.com>
2020-04-25 15:39:28 +0300

    Project built without webpack, just by npm

---
+++ public/precache-manifest.934ee2ddd0dfed9322d9bda9ce2f0f96.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05741c3b6d70ca35d6f42fd2fe02de12",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e0ab7d53a55f460c9b57",
+    "url": "/static/css/main.6d8b5a93.chunk.css"
+  },
+  {
+    "revision": "354336333bc121a2fdd3",
+    "url": "/static/js/2.985a3dd9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.985a3dd9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0ab7d53a55f460c9b57",
+    "url": "/static/js/main.645bfc80.chunk.js"
+  },
+  {
+    "revision": "b4526a8e436e4a595347",
+    "url": "/static/js/runtime-main.3d4cd554.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
